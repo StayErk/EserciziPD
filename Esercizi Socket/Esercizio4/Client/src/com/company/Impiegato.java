9452ea59f857232ae11efc04b4de3b088361acc1
stayerk <edrioe@gmail.com>
2020-10-18 15:28:52 +0200

    Rimossa cartella duplicata

--- Esercizi Socket/Esercizio4/Client/src/com/company/Impiegato.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public interface Impiegato {
-    public String getNome() throws Throwable;
-    public String getIndirizzo() throws Throwable;
-    public int getStipendio() throws Throwable;
-    public int aumentaStipendio(int amount) throws Throwable;
-}
